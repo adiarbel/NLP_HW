c7229b288165eac8872d4af0a895f3bd538698aa
ZOHARRI <zohar.rimon@campus.technion.ac.il>
2019-11-29 16:23:57 +0200

    Zohar: added Stuff

--- Wet1/Code/feature2id_class.py
+++ Wet1/Code/feature2id_class.py
@@ -1,24 +1,32 @@
 from collections import OrderedDict
 import re
 
-class feature_statistics_class():
+class feature2id_class():
 
-    def __init__(self):
+    def __init__(self, feature_statistics, threshold):
+        self.feature_statistics = feature_statistics  # statistics class, for each feature gives empirical counts
+        self.threshold = threshold  # feature count threshold - empirical count must be higher than this
+
         self.n_total_features = 0  # Total number of features accumulated
 
+        self.n_tag_pairs = 0
+        self.n_suffix_tags = 0
+        self.n_prefix_tags = 0
+        self.n_trigram_tags = 0
+        self.n_bigram_tags = 0
+        self.n_unigram_tags = 0
+        self.n_capitalized_tags = 0
+
         # Init all features dictionaries
-        self.words_tags_count_dict = OrderedDict()
-        self.words_suffix_tags_count_dict = OrderedDict()
-        self.words_prefix_tags_count_dict = OrderedDict()
+        self.words_tags_dict = OrderedDict()
+        self.words_suffix_tags_dict = OrderedDict()
+        self.words_prefix_tags_dict = OrderedDict()
+        self.trigram_tags_dict = OrderedDict()
+        self.bigram_tags_dict = OrderedDict()
+        self.unigram_tags_dict = OrderedDict()
+        self.capitalized_tags_dict = OrderedDict()
 
-
-        self.trigram_tags_count_dict = OrderedDict()
-        self.bigram_tags_count_dict = OrderedDict()
-        self.unigram_tags_count_dict = OrderedDict()
-
-        self.capitalized_tags_count_dict = OrderedDict()
-
-    def get_word_tag_pair_count(self, file_path):
+    def get_word_tag_pairs(self, file_path):
         """
             Extract out of text all word/tag pairs
             :param file_path: full path of the file to read
@@ -28,16 +36,16 @@ class feature_statistics_class():
             for line in f:
                 splited_words = re.split(' |\n ', line)
                 del splited_words[-1]
+
                 for word_idx in range(len(splited_words)):
-                    cur_word, cur_tag = splited_words[word_idx].split('_')
-                    if (cur_word, cur_tag) not in self.words_tags_count_dict:
-                        self.words_tags_count_dict[(cur_word, cur_tag)] = 1
-                    else:
-                        self.words_tags_count_dict[(cur_word, cur_tag)] += 1
+                    cur_word, cur_tag = split(splited_words[word_idx], '_')
+                    if ((cur_word, cur_tag) not in self.words_tags_dict) \
+                            and (self.statistics.words_tags_dict[(cur_word, cur_tag)] >= self.threshold):
+                        self.words_tags_dict[(cur_word, cur_tag)] = self.n_total_features
+                        self.n_total_features += 1
+                        self.n_tag_pairs += 1
 
-    # --- ADD YOURE CODE BELOW --- #
-
-    def get_pre_suf_tag_pair_count(self, file_path, pre_suf_flag):
+ def get_pre_suf_tag_pair_count(self, file_path, pre_suf_flag):
         """
             Extract out of text all pre-suf/tag pairs
             :param file_path: full path of the file to read
@@ -59,15 +67,19 @@ class feature_statistics_class():
                         cur_part = cur_word[0:pre_suf_slice_size-1]
                     for i in reversed(range(cur_word_len-1)):
                         if pre_suf_flag:
-                            if (cur_part[-i:-1], cur_tag) not in self.words_suffix_tags_count_dict:
-                                self.words_suffix_tags_count_dict[(cur_part[-i-1:-1], cur_tag)] = 1
-                            else:
-                                self.words_suffix_tags_count_dict[(cur_part[-i-1:-1], cur_tag)] += 1
+                            if (cur_part[-i:-1], cur_tag) not in self.words_suffix_tags_count_dict  \
+                                    and (self.statistics.words_suffix_tags_dict[(cur_part[-i:-1], cur_tag)] >= self.threshold):
+                                self.words_suffix_tags_dict[(cur_part[-i:-1], cur_tag)] = self.n_total_features
+                                self.n_total_features += 1
+                                self.n_suffix_tags += 1
                         else:
-                            if (cur_part[0:i-1], cur_tag) not in self.words_prefix_tags_count_dict:
-                                self.words_prefix_tags_count_dict[(cur_part[0:i], cur_tag)] = 1
-                            else:
-                                self.words_prefix_tags_count_dict[(cur_part[0:i], cur_tag)] += 1
+                            if (cur_part[-i:-1], cur_tag) not in self.words_prefix_tags_count_dict \
+                                    and (self.statistics.words_prefix_tags_dict[
+                                             (cur_part[-i:-1], cur_tag)] >= self.threshold):
+                                self.words_prefix_tags_dict[(cur_part[-i:-1], cur_tag)] = self.n_total_features
+                                self.n_total_features += 1
+                                self.n_prefix_tags += 1
+
 
     def get_trigram_tags_count(self, file_path):
         """
